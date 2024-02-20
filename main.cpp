a95429ca8cdc3cb7bc81c8dfa693382f546e3b60
Ernesto Flores <eflores73@students.avc.edu>
2024-02-20 12:41:23 -0800

    Done

--- main.cpp
+++ main.cpp
@@ -12,4 +12,16 @@ int main()
 	/***************************************************
 	 * Code your program here
 	 **************************************************	*/
+
+	cout << n1 << " " << n2 << " ";
+
+	for (int i = 2; i < N; i++) {
+		int nextNum = n1 + n2;
+		cout << nextNum << " ";
+
+		n1 = n2;
+		n2 = nextNum;
+	}
+
+	return 0;
 }
